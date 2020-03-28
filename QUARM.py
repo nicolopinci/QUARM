4d3eeb64f2731e585e09e98509c019daf4e47bf0
nicolopinci <nicolopinci.1997@gmail.com>
2020-03-28 17:37:00 +0100

    Upload of the Deutsch-Jozsa algorithm

--- QUARM.py
+++ QUARM.py
@@ -5,8 +5,15 @@ Created on Mon Mar 23 20:24:01 2020
 
 @author: nicolo
 """
+
+numbit = 2 # Number of bits
+sortValues = True # decides whether the state values have to be sorted according to probability or not
+
 from plotly.offline import plot
 import plotly.graph_objs as go
+
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
 
 import numpy as np
 from qiskit import IBMQ
@@ -17,22 +24,22 @@ def getIntermediateStatevector(circuit, simulator):
     statevector = result.get_statevector(circuit)
     return statevector
 
-def getProbabilityVector(statevector):
+def getProbabilityVector(statevector): # for every state, gets the probability associated to it
     return np.square(np.abs(statevector)).tolist()
     
 def getProbabilities(circuit, simulator):
     return getProbabilityVector(getIntermediateStatevector(circuit, simulator))
 
-def addToPlot(timeMatrix, circuit, simulator):
+def addToPlot(timeMatrix, circuit, simulator): # adds to the final matrix, that will be used for the plot
     timeMatrix.append(getProbabilities(circuit, simulator))
    
-def transpose(matrix):
+def transpose(matrix): # matrix transposition to have time on the x axis
     numpy_array = np.array(matrix)
     transpose = numpy_array.T
     transpose_list = transpose.tolist()
     return transpose_list
 
-def sort(matrix):
+def sort(matrix): # sort starting from the highest probability to the lowest
     sortedMatrix = matrix.copy()
     for i in range(0, len(sortedMatrix)):
         sortedMatrix[i].sort(reverse = True)
@@ -42,26 +49,24 @@ def sort(matrix):
 def convertToBinary(x, n=0): # from https://stackoverflow.com/questions/699866/python-int-to-binary-string
     return format(x, 'b').zfill(n)
 
+# GUI definition
+# Upload a file
+Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+
+file = open(filename, "r")
+code = file.read()
+
+n = 2
+
+code = "\naddToPlot(timeMatrix, circuit, simulator)\n".join(code.split("\n"))
+
 timeMatrix = []
-numbit = 2
-sortValues = False
 
 simulator = Aer.get_backend('statevector_simulator')
 
-# Create a Quantum Circuit acting on the q register
-circuit = QuantumCircuit(numbit, numbit)
-
-# Add a H gate on qubit 0
-circuit.h(0)
-addToPlot(timeMatrix, circuit, simulator)
-
-# Add a CX (CNOT) gate on control qubit 0 and target qubit 1
-circuit.cx(0, 1)
-addToPlot(timeMatrix, circuit, simulator)
- 
-# Map the quantum measurement to the classical bits
-circuit.measure([0,1], [0,1])
-addToPlot(timeMatrix, circuit, simulator)
+# Execute the quantum code simulation
+exec(code)
 
 if(sortValues == True):
     timeMatrix = sort(timeMatrix)
